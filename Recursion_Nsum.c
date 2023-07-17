4f9d6355ca041cacd83853fc7be326448f9a09d1
shivmSingh1 <shivams7380@gmai.com>
2023-07-18 02:53:02 +0530

    sum of N natural number by Recursion

---
+++ Recursion_Nsum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum(int n);
+int main(){
+
+	int n;
+	printf("enter number ");
+	scanf("%d", &n);
+
+
+printf("sum of numbers is %d ",sum(n));
+
+	return 0;
+}
+ int sum(int n){
+if(n == 1){
+
+	return 1;
+}
+ 	int sumNm1= sum(n-1);
+ 	int sumN= sumNm1 + n;
+ 	return sumN;
+ }
