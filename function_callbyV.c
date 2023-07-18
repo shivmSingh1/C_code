f83a5ec5d023c017355ff89a918b0ec107bf6872
shivmSingh1 <shivams7380@gmai.com>
2023-07-18 21:14:17 +0530

    Example of call by value

---
+++ function_callbyV.c
@@ -0,0 +1,26 @@
+#include <stdio.H>
+int increment(int a, int b);
+int main(){
+
+
+	int a=4,b=2;
+
+	increment(a,b);
+
+
+	printf("value of a and b is equal to %d , %d \n", a,b);
+
+
+	return 0;
+}
+
+int increment(int a, int b){
+
+	a++;
+	b++;
+
+	printf("value of a and b is equal to %d , %d \n", a,b);
+
+return a,b;
+
+}
