b16d776e15fd906b2f1683ea82b7fdeb4f51f28a
shivmSingh1 <shivams7380@gmai.com>
2023-07-26 05:50:19 +0530

    increase the value of pointer and track next variable address

---
+++ Pointer_arthmetic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+int i=67;
+
+int *ptr= &i;
+
+/*printf("the value of ptr is %u ", ptr);
+
+ptr++;
+
+printf("\nthe value of ptr is %u ", ptr); */
+
+/* char j='a';
+char *ptrr=&j;
+
+printf("the value of ptrr is %u ",ptrr);
+ptrr++;
+printf("\nthe value of ptrr is %u ",ptrr); */
+
+/* float k=34.56;
+float *ptrrr=&k;
+
+printf("the value of ptrr is %u ",ptrrr);
+ptrrr++;
+printf("\nthe value of ptrr is %u ",ptrrr); */
+
+double m='a';
+double *ptrrrr=&m;
+
+printf("the value of ptrr is %u ",ptrrrr);
+ptrrrr++;
+printf("\nthe value of ptrr is %u ",ptrrrr);
+
+	return 0;
+}
